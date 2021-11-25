6d57ab13febff3c8689cc3854875acac82e1d97d
Gabriel Mello <cr.macedo21@gmail.com>
2021-11-25 15:50:33 -0300

    Curso Microservices

---
+++ 06-cambio-service/src/main/java/br/com/mello/repository/CambioRepository.java
@@ -0,0 +1,11 @@
+package br.com.mello.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.mello.model.Cambio;
+
+public interface CambioRepository extends JpaRepository<Cambio, Long>{
+
+	Cambio findByFromAndTo(String from, String to);
+	
+}
